98a7b072685ce30a18712a5a216cc670374e72ad
Helen Pitt <helenpitt@Helens-MacBook-Pro.local>
2017-10-06 21:19:26 -0400

    almost there

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -1,43 +1,52 @@
 //Letter choices available
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+//Computer Generated random letters.
+letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+console.log("letter to guess: " + letterToGuess);
+
 //Setting all to zero
 var wins = 0;
 var losses = 0;
 var guesses = 0;
-var guessesLeft = 9;
-var guessedLetters = [];
+var guessesLeft = 7;
+var guessedLetters ;
 var letterToGuess = [];
 
 
 
 //Lets the computer select a random letter from the available choices
 
-//Displaying the guessed letters
+//Functions for back-end workings of the game.
 document.onkeyup = function (event) {
   var guesses = event.key;
+  guessesLeft = guessesLeft - 1;
+  guessedLetters = display();
   if (guesses === letterToGuess) {
     wins();
-  } else(guessesLeft - 1 === 0)
-  lost();
+  } else if (guessesLeft - 1 === 0) {
+    lost();
+  } else {
+    display();
+  }
 
-  display();
 }
 
 // Function to have the guesses displayed
 function display() {
   var winsP = document.getElementById("wins");
   var losesP = document.getElementById("losses");
-  var guessLeft = document.getElementById("guessLeft");
+  var guessLeft = document.getElementById("guessesLeft");
   var guessedLetters = document.getElementById("guessed");
   winsP.innerHTML = wins;
   losesP.innerHTML = losses;
   // these are bringing up errors in the console. Not sure I know why.
   guessLeft.innerHTML = guessesLeft;
-  letterGuessed.innerHTML = guessedLetters.join(',');
+  guessedLetters.innerHTML = guessedLetters ;
 }
 
-function win() {
+// Outcomes of the game/ calls from line 20.
+function wins() {
   wins++;
   resetGame();
 }
@@ -46,10 +55,8 @@ function lost() {
   losses++;
   resetGame();
 }
-// Generates the random number.
+// Function to reset the game.
 function resetGame() {
   guessesLeft = 7;
   guessedLetters = [];
-  letterToGuess = letters[Math.floor(Math.random() * letters.length)];
-  console.log("letter to guess: " + letterToGuess);
 }
